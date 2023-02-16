a733fa459de187079da812cd7fb14f669b6be771
lajarre <alexandre.hajjar@gmail.com>
2023-02-16 16:43:20 +0000

    ✨ Subtyping coercion for Records

--- src/SubtypingCoercion.hs
+++ src/SubtypingCoercion.hs
@@ -10,7 +10,7 @@ data Type = TyRecord [(String, Type)]
           | TyTop
           | TyArr Type Type
           | TyUnit
-        deriving (Show, Eq)
+        deriving (Eq)
 
 data Binding = NameBind
              | VarBind Type
@@ -22,14 +22,38 @@ data Term = TmRecord [(String, Term)]
           | TmAbs String Type Term
           | TmApp Term Term
           | TmUnit
-        deriving (Eq, Show)
+        deriving (Eq)
 
 type Context = [(String, Binding)]
-type ContextPrefix = [(String, Binding)]
 
 data Typing = TypeError String 
             | Type (Type, Term)
             deriving (Eq, Show)
+
+printTy :: Type -> String
+printTy TyTop = "T"
+printTy TyUnit = "U"
+printTy (TyArr t1 t2) = printf "(%s → %s)" (printTy t1) (printTy t2)
+printTy (TyRecord types) = printf $ "{" ++ foldl (\ acc (l, t) -> (if acc == "" then "" else acc ++ ", ") ++ l ++ ": " ++ printTy t) "" types ++ "}"
+
+instance Show Type where
+  show = printTy
+
+printTm :: Context -> Term -> String
+printTm ctx (TmAbs x tyX t1) =
+  let (ctx', x') = pickFreshName ctx x in
+  printf "(lambda %s: %s. %s)" x' (show tyX) (printTm ctx' t1)
+printTm ctx (TmApp t1 t2) =
+  printf "(%s %s)" (printTm ctx t1) (printTm ctx t2)
+printTm ctx (TmVar i n) =
+  if length ctx == n
+     then printf "%s" (fst (ctx !! i))
+     else printf "[bad index]"
+printTm ctx TmUnit = printf "unit"
+
+instance Show Term where
+  show = printTm []
+
 
 pickFreshName :: Context -> String -> (Context, String)
 pickFreshName ctx x =
@@ -60,7 +84,7 @@ coerce TyUnit = TyUnit
 subtype :: Context -> Type -> Type -> Either String (Context, Term)
 subtype ctx tyS tyT |
   tyS == tyT =
-    Right (ctx', TmAbs var tyS (TmVar 0 1))
+    Right (ctx', TmAbs var tyS (TmVar 0 (length ctx')))
     where
       var = "x'coersametype"
       ctx' = addBinding ctx var (VarBind tyS)
@@ -70,16 +94,17 @@ subtype ctx tyS TyTop =
           ctx' = addBinding ctx var (VarBind tyS)
 subtype ctx (TyArr s1't s2't) (TyArr t1't t2't) =
   case (subtype ctx t1't s1't, subtype ctx s2't t2't) of
-    (Left errMsg1, _)                               -> Left $ "Arrow applicator subtype error:\n\t" ++ errMsg1
-    (_, Left errMsg2)                               -> Left $ "Arrow argument subtype error:\n\t" ++ errMsg2
-    (Right (ctx1, t1_s1's), Right (ctx2, s2_t2's))  -> Right
+    (Left errMsg1, _)                               -> Left $ "Arrow subtype image error: " ++ errMsg1
+    (_, Left errMsg2)                               -> Left $ "Arrow subtype domain error: " ++ errMsg2
+    (Right (ctx1, t1_s1's), Right (ctx2, s2_t2's))  ->
+      Right
       (
         ctx'',
         TmAbs f s1_s2'c $
           TmAbs x t1'c $
             TmApp s2_t2's $
-              TmApp (TmVar 1 2) $
-                TmApp t1_s1's (TmVar 0 2)
+              TmApp (TmVar 1 (length ctx')) $
+                TmApp t1_s1's (TmVar 0 (length ctx''))
       )
         where f = "f'coerapp"
               x = "x'coerapp"
@@ -87,79 +112,39 @@ subtype ctx (TyArr s1't s2't) (TyArr t1't t2't) =
               t1'c = coerce t1't
               ctx' = addBinding ctx f (VarBind s1_s2'c)
               ctx'' = addBinding ctx' x (VarBind t1'c)
--- subtype ctx (TyRecord fS) (TyRecord fT) =
---   SubtypeCoercion r [(kj, snd (typeOf fSj))] [(li, (subtype fSi fTi) (lookup li r)]
---   where (ctx', r) = pickFreshName ctx "subtypecoerRecord"
--- 
---   foldl
---   (\ acc (li, tyTi) -> 
---     case (acc, lookup li fS) of
---       (Nothing, Nothing)    -> Left $ "Subtyping error: record doesn't contain label(s): " ++ li
---       (Just err, Nothing)   -> Left $ err ++ ", " ++ li
---       (Nothing, Just tySi)  -> subtype tySi tyTi
---       (Just err, Just tySi) ->
---         maybe
---         (Just err)
---         (\ subErr -> Just $ err ++ ". With errors in item subtyping.\n" ++ subErr)
---         (subtype tySi tyTi)
---   )
---   ????
---   fT
-subtype _ _ _ = Left "Subtyping error" -- [XXX]
--- subtype _ TyBot _ = Nothing
--- subtype (TyRecord fS) (TyRecord fT) =
---   foldl
---   (\ acc (li, tyTi) -> 
---     case (acc, lookup li fS) of
---       (Nothing, Nothing)    -> Just $ "Subtyping error: record doesn't contain label(s): " ++ li
---       (Just err, Nothing)   -> Just $ err ++ ", " ++ li
---       (Nothing, Just tySi)  -> subtype tySi tyTi
---       (Just err, Just tySi) ->
---         maybe
---         (Just err)
---         (\ subErr -> Just $ err ++ ". With errors in item subtyping.\n" ++ subErr)
---         (subtype tySi tyTi)
---   )
---   Nothing
---   fT
--- subtype (TyArr tyS1 tyS2) (TyArr tyT1 tyT2) =
---   case (subtype tyS1 tyT1, subtype tyS2 tyT2) of
---     (Nothing, Nothing) -> Nothing
---     (Just err, Nothing) -> Just $ "Subtyping error: Arrow type domain error\n" ++ err
---     (Nothing, Just err) -> Just $ "Subtyping error: Arrow type image error\n" ++ err
---     (Just err1, Just err2) -> Just $ "Subtyping error: Arrow type error domain and image error\n -" ++ err1 ++ "\n -" ++ err2
--- subtype _ _ = Just "Subtyping error: types mismatch"
+subtype ctx (TyRecord fs) (TyRecord ft) =
+  case
+    foldl
+    (\ acc (li, fsi't) -> 
+      case (acc, lookup li ft) of
+        (Left errMsg, Nothing)  -> Left $ errMsg ++ " Missing field: " ++ li ++ "."
+        (_, Nothing)            -> Left $ "Missing field: " ++ li ++ "."
+        (acc, Just fti't) ->
+          case (acc, subtype ctx fsi't fti't) of
+            (Left errMsg, Left errMsgi) -> Left $ errMsg ++ " Subtype coercion error, field: " ++ li ++ "."
+            (_, Left errMsgi)           -> Left $ "Subtype coercion error, field: " ++ li ++ "."
+            (Left errMsg, _)            -> Left errMsg
+            (Right accTerms, Right (_, coerTerm)) ->
+              Right $ accTerms ++ [(li, TmApp coerTerm (TmProj (TmVar 0 1) li))]
+    )
+    (Right [])
+    fs
+  of
+    Left errMsg       -> Left $ "RECORD SUBTYPING ERROR: " ++ errMsg
+    Right coerTerms'  -> Right (ctx, TmAbs "r'coerrec" (TyRecord fs) (TmRecord coerTerms'))
+subtype _ _ _ = Left "Subtyping error"
 
 
-replaceAbsType :: Term -> Type -> Term
-replaceAbsType (TmAbs x tyX t1) ty = TmAbs x ty t1
-replaceAbsType term _ = term
+termShift :: Int -> Term -> Term
+termShift d = walk 0 where
+  walk :: Int -> Term -> Term
+  walk c (TmVar i n) = if i >= c then TmVar (i + d) (n + d) else TmVar i (n + d)
+  walk c (TmAbs x tyT1 t2) = TmAbs x tyT1 (walk (c + 1) t2)
+  walk c (TmApp t1 t2) = TmApp (walk c t1) (walk c t2)
+  walk c (TmRecord entries) = TmRecord $ map (second (walk c)) entries
+  walk c a = a
 
 typeOf :: Context -> Term -> Typing
--- typeOf ctx (TmRecord fields) =
---   either
---   TypeError 
---   (Type . TyRecord)
---   (
---     foldl 
---     (\ acc (li, fi) ->
---       case (acc, typeOf ctx fi) of 
---         (Left err, TypeError erri)      -> Left $ err ++ ", on label " ++ li ++ ": " ++ erri
---         (_, TypeError erri)             -> Left $ "Record typing error on label " ++ li ++ ": " ++ erri
---         (Right accFieldTypes, Type fti) -> Right $ accFieldTypes ++ [(li, fti)]
---     )
---     (Right [])
---     fields
---   )
--- typeOf ctx (TmProj t1 l) =
---   case typeOf ctx t1 of
---     Type (TyRecord fieldTypes)  ->
---       case typeLookup of
---         Just typ -> Type typ
---         Nothing -> TypeError $ "projection on non-existing label " ++ show l
---       where typeLookup = lookup l fieldTypes
---     TypeError errMsg            -> TypeError errMsg
---     _                           -> TypeError "projection on non-record"
 typeOf ctx (TmVar i offset) = 
   case getTypeFromContext ctx i of
     Just ty -> Type (ty, TmVar i offset)
@@ -178,17 +163,32 @@ typeOf ctx (TmApp t1 t2) =
         Left errMsg             -> TypeError $ "Parameter type mismatch:\n\t" ++ errMsg 
         Right (ctx', t2_t11's)  -> Type (t12't, TmApp t1'd (TmApp t2_t11's t2'd))
     _                                                   -> TypeError "Applicator not arrow type"
--- typeOf ctx (TmApp (TmAbs t1x t1XType t1Term) t2) =
---   case typeOf ctx t2 of
---     TypeError errMsg    -> TypeError $ "Right term type error in App:\n\t" ++ errMsg
---     Type (tyT2, coerT2) ->
---       case subtype ctx tyT2 t1XType of
---         Left subtypeErr ->
---           TypeError $ "Parameter type mismatch:\n\t" ++ subtypeErr
---         Right (ctx', SubtypeCoercion coerX coerXType coerTerm) ->
---           typeOf ctx' $ -- [xxx] sure?
---             TmApp
---             (TmAbs coerX coerXType coerTerm)
---             (TmApp (TmAbs t1x tyT2 t1Term) t2)
---             -- No. t1Term needs coercion as well.
+typeOf ctx (TmRecord fields) =
+  either
+  TypeError
+  (\ (x, y) -> Type (TyRecord x, TmRecord y))
+  (
+    foldl
+    (\ acc (li, fi) -> 
+      case (acc, typeOf ctx fi) of
+        (Left err, TypeError erri)  -> Left $ err ++ ", on label " ++ li ++ ": " ++ erri
+        (Left err, _)               -> Left err
+        (_, TypeError erri)         -> Left $ "Record typing error on label " ++ li ++ ": " ++ erri
+        (Right (accFieldTypes, accFieldCoercions), Type (fi't, fi'c))
+                                    -> Right (accFieldTypes ++ [(li, fi't)], accFieldCoercions ++ [(li, fi'c)])
+    )
+    (Right ([], []))
+    fields
+  )
+typeOf ctx (TmProj t l) =
+  case typeOf ctx t of
+    Type (TyRecord t't, TmRecord t'c) ->
+      case (lookup l t't, lookup l t'c) of
+        (Just ti't, Just ti'c) -> Type (ti't, ti'c)
+        (Just ti't, _)         -> TypeError "Projection on record with type/coercion mismatch" -- should not happen
+        (_, Just ti'c)         -> TypeError "Projection on record with type/coercion mismatch" -- should not happen
+        _                      -> TypeError $ "Projection on non-existing label " ++ show l
+    Type _            -> TypeError "Projection on non-record"
+    TypeError errMsg  -> TypeError errMsg
+
 typeOf ctx TmUnit = Type (TyUnit, TmUnit)
